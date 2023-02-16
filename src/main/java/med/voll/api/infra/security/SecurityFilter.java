e15377ab62342d64196a90851cc8c2146bb0199c
Matielo J Geronimo <matielo.dev@gmail.com>
2023-02-16 00:53:23 -0300

    Retrieving the token in the request

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -11,15 +11,24 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class SecurityFilter extends OncePerRequestFilter{
+public class SecurityFilter extends OncePerRequestFilter {
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+
+		var tokenJWT = recuperarToken(request);
 		filterChain.doFilter(request, response);
 	}
-	
+
+	private String recuperarToken(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		var authorizationHeader = request.getHeader("Authorization");
+		if (authorizationHeader == null) {
+			throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+		}
+		return authorizationHeader.replace("Bearer ","");
+	}
 
 }
