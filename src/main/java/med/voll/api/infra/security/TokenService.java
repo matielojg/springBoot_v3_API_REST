e15dba54df0d849020674d3f47ef9e5fbb381dc8
Matielo J Geronimo <matielo.dev@gmail.com>
2023-02-16 00:25:55 -0300

    Token parameterization as DTO

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
@@ -14,28 +15,29 @@ import med.voll.api.domain.usuario.Usuario;
 
 @Service
 public class TokenService {
-	
+
+	@Value("${api.security.token.secret}")
+	private String secret;
+
 	/**
-	 * Criado com a referencia do git abaixo
-	 * https://github.com/auth0/java-jwt
+	 * Criado com a referencia do git abaixo https://github.com/auth0/java-jwt
+	 * 
 	 * @param usuario
 	 * @return
 	 */
-	
-	 public String gerarToken(Usuario usuario) { 
-	        try {
-	            var algoritmo = Algorithm.HMAC256("12345678");
-	            return JWT.create()
-	                .withIssuer("API Voll.med")
-	                .withSubject(usuario.getLogin())
-	                .withExpiresAt(dataExpiracao())
-	                .sign(algoritmo);
-	        } catch (JWTCreationException exception){
-	            throw new RuntimeException("erro ao gerrar token jwt", exception);
-	        }        
-	    }
 
-	    private Instant dataExpiracao() {
-	        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
-	    }
+	public String gerarToken(Usuario usuario) {
+		System.out.println(secret);
+		try {
+			var algoritmo = Algorithm.HMAC256(secret);
+			return JWT.create().withIssuer("API Voll.med").withSubject(usuario.getLogin())
+					.withExpiresAt(dataExpiracao()).sign(algoritmo);
+		} catch (JWTCreationException exception) {
+			throw new RuntimeException("erro ao gerrar token jwt", exception);
+		}
 	}
+
+	private Instant dataExpiracao() {
+		return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+	}
+}
